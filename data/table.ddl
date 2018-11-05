c2d7ed40c35b4d680f2ab4ddcea0746f7740a59e
xiaran <xiaran@polomi.com>
2018-11-05 21:00:00 +0800

    update

--- data/table.ddl
+++ data/table.ddl
@@ -1,11 +1,9 @@
-CREATE TABLE IF NOT EXISTS `imock_log`(
-   `id` INT UNSIGNED AUTO_INCREMENT,
-   `page_id` VARCHAR(200),
-   `time` VARCHAR(200),
-   `name` VARCHAR(200),
-   `person` VARCHAR(200),
-   `difficulty` VARCHAR(200),
-   `duration` VARCHAR(200),
-   `score` VARCHAR(200),
-   PRIMARY KEY ( `id` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS imooc_log(
+   page_id VARCHAR(200),
+   time VARCHAR(200),
+   name VARCHAR(200),
+   person VARCHAR(200),
+   difficulty VARCHAR(200),
+   duration VARCHAR(200),
+   score VARCHAR(200)
+)
--- etl.js
+++ etl.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var TEST_DATABASE = 'imooc';
-var TEST_TABLE = 'imock_log';
+var TEST_TABLE = 'imooc_log';
 
 //创建连接 
 
@@ -11,21 +11,38 @@ var client = mysql.createConnection({
 
 client.connect();
 client.query("use " + TEST_DATABASE);
+createTable()
+insertTable()
 
 
-function createTable(){
-
-
-
+function createTable() {
+    var sql = `
+    CREATE TABLE IF NOT EXISTS imooc_log(
+        page_id VARCHAR(200),
+        time VARCHAR(200),
+        name VARCHAR(200),
+        person VARCHAR(200),
+        difficulty VARCHAR(200),
+        duration VARCHAR(200),
+        score VARCHAR(200)
+     )
+    `
+    client.query(sql, null, function (err, result) {
+        if (err) {
+            console.log('[CREATE ERROR] - ', err.message);
+            return;
+        }
+        console.log('CREATE ID:', result);
+    });
 }
 
 
 
 function insertTable() {
-    var userAddSql = 'INSERT INTO node_user(id,name,age) VALUES(0,?,?)';
-    var userAddSql_Params = ['Wilson', 55];
+    var userAddSql = 'INSERT INTO ' + TEST_TABLE + ' VALUES(?,?,?,?,?,?,?)';
+    var userAddSql_Params = ["295", "2018-11-05 19:44:50", "Go语言实战抽奖系统", "28", "中级", "14小时", "10.00分"];
     //增 ad
-    connection.query(userAddSql, userAddSql_Params, function (err, result) {
+    client.query(userAddSql, userAddSql_Params, function (err, result) {
         if (err) {
             console.log('[INSERT ERROR] - ', err.message);
             return;
